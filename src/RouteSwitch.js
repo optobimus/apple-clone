aa80428a22068fb3886dc3c346c7aa943ad4da81
Tobias <113835194+optobimus@users.noreply.github.com>
2023-03-31 21:43:50 +0200

    Complete React setup + create nav component

---
+++ src/RouteSwitch.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+
+const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  );
+};
+
+export default RouteSwitch;
